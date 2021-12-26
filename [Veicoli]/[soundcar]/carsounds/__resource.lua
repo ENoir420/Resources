20b8c0118998d6873b2fdb190f0b08af49b5383f
ENoir420 <70651562+ENoir420@users.noreply.github.com>
2021-12-26 16:22:48 +0100

    First Commit
    Signed-off-by: ENoir420 <70651562+ENoir420@users.noreply.github.com>

---
+++ [Veicoli]/[soundcar]/carsounds/__resource.lua
@@ -0,0 +1,183 @@
+resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
+
+files {
+  'audioconfig/mclarenv8_game.dat',
+  'audioconfig/mclarenv8_game.dat151.nametable',
+  'audioconfig/mclarenv8_game.dat151.rel',
+  'audioconfig/mclarenv8_sounds.dat',
+  'audioconfig/mclarenv8_sounds.dat54.nametable',
+  'audioconfig/mclarenv8_sounds.dat54.rel',
+  'sfx/dlc_mclarenv8/mclarenv8.awc',
+  'sfx/dlc_mclarenv8/mclarenv8_npc.awc',
+  'audioconfig/f40v8_game.dat',
+  'audioconfig/f40v8_game.dat151.nametable',
+  'audioconfig/f40v8_game.dat151.rel',
+  'audioconfig/f40v8_sounds.dat',
+  'audioconfig/f40v8_sounds.dat54.nametable',
+  'audioconfig/f40v8_sounds.dat54.rel',
+  'sfx/dlc_f40v8/f40v8.awc',
+  'sfx/dlc_f40v8/f40v8_npc.awc',
+  'audioconfig/apollosv8_game.dat',
+  'audioconfig/apollosv8_game.dat151.nametable',
+  'audioconfig/apollosv8_game.dat151.rel',
+  'audioconfig/apollosv8_sounds.dat',
+  'audioconfig/apollosv8_sounds.dat54.nametable',
+  'audioconfig/apollosv8_sounds.dat54.rel',
+  'sfx/dlc_apollosv8/apollosv8.awc',
+  'sfx/dlc_apollosv8/apollosv8_npc.awc',
+  'audioconfig/ferrarif12_game.dat',
+  'audioconfig/ferrarif12_game.dat151.nametable',
+  'audioconfig/ferrarif12_game.dat151.rel',
+  'audioconfig/ferrarif12_sounds.dat',
+  'audioconfig/ferrarif12_sounds.dat54.nametable',
+  'audioconfig/ferrarif12_sounds.dat54.rel',
+  'sfx/dlc_ferrarif12/ferrarif12.awc',
+  'sfx/dlc_ferrarif12/ferrarif12_npc.awc',
+  'audioconfig/c6v8sound_game.dat',
+  'audioconfig/c6v8sound_game.dat151.nametable',
+  'audioconfig/c6v8sound_game.dat151.rel',
+  'audioconfig/c6v8sound_sounds.dat',
+  'audioconfig/c6v8sound_sounds.dat54.nametable',
+  'audioconfig/c6v8sound_sounds.dat54.rel',
+  'sfx/dlc_c6v8sound/c6v8sound.awc',
+  'sfx/dlc_c6v8sound/c6v8sound_npc.awc',
+  'audioconfig/diablov12_game.dat',
+  'audioconfig/diablov12_game.dat151.nametable',
+  'audioconfig/diablov12_game.dat151.rel',
+  'audioconfig/diablov12_sounds.dat',
+  'audioconfig/diablov12_sounds.dat54.nametable',
+  'audioconfig/diablov12_sounds.dat54.rel',
+  'sfx/dlc_diablov12/diablov12.awc',
+  'sfx/dlc_diablov12/diablov12_npc.awc',
+  'audioconfig/elegyx_game.dat151',
+  'audioconfig/elegyx_game.dat151.nametable',
+  'audioconfig/elegyx_game.dat151.rel',
+  'audioconfig/elegyx_sounds.dat54',
+  'audioconfig/elegyx_sounds.dat54.nametable',
+  'audioconfig/elegyx_sounds.dat54.rel',
+  'sfx/dlc_elegyx/elegyx.awc',
+  'sfx/dlc_elegyx/elegyx_npc.awc',
+  'audioconfig/ea825_game.dat',
+  'audioconfig/ea825_game.dat151.nametable',
+  'audioconfig/ea825_game.dat151.rel',
+  'audioconfig/ea825_sounds.dat',
+  'audioconfig/ea825_sounds.dat54.nametable',
+  'audioconfig/ea825_sounds.dat54.rel',
+  'sfx/dlc_ea825/ea825.awc',
+  'sfx/dlc_ea825/ea825_npc.awc',
+  'audioconfig/mercm177_game',
+  'audioconfig/mercm177_game.dat151.nametable',
+  'audioconfig/mercm177_game.dat151.rel',
+  'audioconfig/mercm177_sounds',
+  'audioconfig/mercm177_sounds.dat54.nametable',
+  'audioconfig/mercm177_sounds.dat54.rel',
+  'sfx/dlc_mercm177/mercm177.awc',
+  'sfx/dlc_mercm177/mercm177_npc.awc',
+  'audioconfig/lambov10_game',
+  'audioconfig/lambov10_game.dat151.nametable',
+  'audioconfig/lambov10_game.dat151.rel',
+  'audioconfig/lambov10_sounds',
+  'audioconfig/lambov10_sounds.dat54.nametable',
+  'audioconfig/lambov10_sounds.dat54.rel',
+  'sfx/dlc_lambov10/lambov10.awc',
+  'sfx/dlc_lambov10/lambov10_npc.awc',
+  'audioconfig/s65b40_game',
+  'audioconfig/s65b40_game.dat151.nametable',
+  'audioconfig/s65b40_game.dat151.rel',
+  'audioconfig/s65b40_sounds',
+  'audioconfig/s65b40_sounds.dat54.nametable',
+  'audioconfig/s65b40_sounds.dat54.rel',
+  'sfx/dlc_s65b40/s65b40.awc',
+  'sfx/dlc_s65b40/s65b40_npc.awc',
+  'audioconfig/mercm279_game',
+  'audioconfig/mercm279_game.dat151.nametable',
+  'audioconfig/mercm279_game.dat151.rel',
+  'audioconfig/mercm279_sounds',
+  'audioconfig/mercm279_sounds.dat54.nametable',
+  'audioconfig/mercm279_sounds.dat54.rel',
+  'sfx/dlc_mercm279/mercm279.awc',
+  'sfx/dlc_mercm279/mercm279_npc.awc',
+  'audioconfig/s65b40_game',
+  'audioconfig/s65b40_game.dat151.nametable',
+  'audioconfig/s65b40_game.dat151.rel',
+  'audioconfig/s65b40_sounds',
+  'audioconfig/s65b40_sounds.dat54.nametable',
+  'audioconfig/s65b40_sounds.dat54.rel',
+  'sfx/dlc_s65b40/s65b40.awc',
+  'sfx/dlc_s65b40/s65b40_npc.awc',
+  'audioconfig/porschema2_game',
+  'audioconfig/porschema2_game.dat151.nametable',
+  'audioconfig/porschema2_game.dat151.rel',
+  'audioconfig/porschema2_sounds',
+  'audioconfig/porschema2_sounds.dat54.nametable',
+  'audioconfig/porschema2_sounds.dat54.rel',
+  'sfx/dlc_porschema2/porschema2.awc',
+  'sfx/dlc_porschema2/porschema2_npc.awc',
+  'audioconfig/b58b30_game',
+  'audioconfig/b58b30_game.dat151.nametable',
+  'audioconfig/b58b30_game.dat151.rel',
+  'audioconfig/b58b30_sounds',
+  'audioconfig/b58b30_sounds.dat54.nametable',
+  'audioconfig/b58b30_sounds.dat54.rel',
+  'sfx/dlc_b58b30/b58b30.awc',
+  'sfx/dlc_b58b30/b58b30_npc.awc',
+  'audioconfig/rb26dett_game',
+  'audioconfig/rb26dett_game.dat151.nametable',
+  'audioconfig/rb26dett_game.dat151.rel',
+  'audioconfig/rb26dett_sounds',
+  'audioconfig/rb26dett_sounds.dat54.nametable',
+  'audioconfig/rb26dett_sounds.dat54.rel',
+  'sfx/dlc_rb26dett/rb26dett.awc',
+  'sfx/dlc_rb26dett/rb26dett_npc.awc',
+  
+}
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/mclarenv8_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/mclarenv8_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_mclarenv8'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/f40v8_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/f40v8_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_f40v8'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/apollosv8_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/apollosv8_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_apollosv8'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/ferrarif12_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/ferrarif12_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_ferrarif12'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/diablov12_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/diablov12_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_diablov12'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/c6v8sound_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/c6v8sound_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_c6v8sound'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/elegyx_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/elegyx_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_elegyx'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/ea825_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/ea825_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_ea825'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/mercm177_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/mercm177_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_mercm177'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/lambov10_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/lambov10_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_lambov10'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/s65b40_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/s65b40_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_s65b40'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/mercm279_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/mercm279_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_mercm279'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/s65b40_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/s65b40_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_s65b40'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/porschema2_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/porschema2_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_porschema2'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/rb26dett_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/rb26dett_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_rb26dett'
+
+client_script {
+    'vehicle_names.lua'
+}
