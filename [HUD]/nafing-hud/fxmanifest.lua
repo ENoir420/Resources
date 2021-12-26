c6953f0a826e2cfddcdc13395ec52e5a6e0a109f
ENoir420 <70651562+ENoir420@users.noreply.github.com>
2021-12-26 16:40:08 +0100

    FIrst Commit
    Signed-off-by: ENoir420 <70651562+ENoir420@users.noreply.github.com>

---
+++ [HUD]/nafing-hud/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+version '1.0.1'
+description 'nafing Hud DC: ✪ dydolek#2137'
+
+ui_page 'html/ui.html'
+
+client_scripts {
+	'client.lua'
+}
+
+files {
+	'html/ui.html',
+	'html/style.css',
+	'html/main.js',
+	'html/img/*.png',
+}
